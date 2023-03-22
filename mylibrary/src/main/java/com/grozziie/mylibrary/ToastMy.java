455f57beb4a561e1625477bf478f5aa2357cb8cf
Arifuljava <arifulislam.java.c@gmail.com>
2023-03-22 16:29:29 +0600

    master

---
+++ mylibrary/src/main/java/com/grozziie/mylibrary/ToastMy.java
@@ -0,0 +1,10 @@
+package com.grozziie.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMy {
+    private static  void MyText(Context context,String mes) {
+        Toast.makeText(context, "DDDD", Toast.LENGTH_SHORT).show();
+    }
+}
